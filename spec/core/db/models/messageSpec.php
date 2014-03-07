4e9de95e633b9fb7cf0686daf7e9b3c461420188
dariush <b.g.dariush@gmail.com>
2014-03-07 10:41:37 +0330

    @ spec/core/db/models/messageSpec.php: done

--- spec/core/db/models/messageSpec.php
+++ spec/core/db/models/messageSpec.php
@@ -11,5 +11,15 @@ class messageSpec extends \PhpSpec\ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('\core\db\models\message');
+        # no initial message
+        $this->count()->shouldBeLike(0);
+    }
+    function it_sends_messages()
+    {
+        $faker = \Faker\Factory::create();
+        $faker->seed(time());
+        $this->shouldThrow(new \zinux\kernel\exceptions\dbException("Message can't be blank"))->duringSend(\spec\core\db\models\userSpec::getUser()->user_id, \spec\core\db\models\userSpec::getUser(\spec\core\db\models\userSpec::USER_ONE)->user_id, NULL);
+        $this->shouldNotThrow(new \zinux\kernel\exceptions\dbException("Message can't be blank"))->duringSend(\spec\core\db\models\userSpec::getUser()->user_id, \spec\core\db\models\userSpec::getUser(\spec\core\db\models\userSpec::USER_ONE)->user_id, implode(PHP_EOL, $faker->paragraphs));
+        $this->count()->shouldBeLike(1);
     }
 }
